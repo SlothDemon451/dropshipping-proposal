083dbcf7707c9860b353bb478228a9117230fdd0
Usman Imran <usmanimran158@gmail.com>
2024-12-17 20:04:49 +0500

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Script from 'next/script'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -32,6 +33,7 @@ export default function RootLayout({
 
    </head>
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   )
 }
